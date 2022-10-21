dc2c4d6b5e6121250a3c08b63390be2ca5c740d7
João Victor Santana Ferreira <joaovictorsantanaf@gmail.com>
2022-10-21 18:21:09 -0300

    flow/grind/layered layout

--- src/br/joaovictor/swing/button/Main.java
+++ src/br/joaovictor/swing/button/Main.java
@@ -1,2 +1,8 @@
-package br.joaovictor.swing.button;public class Main {
+package br.joaovictor.swing.button;
+
+public class Main {
+    public static void main(String[] args) {
+        // JButton =  um botão que performa uma ação quando clicado
+        new MyFrame();
+    }
 }
--- src/br/joaovictor/swing/button/MyFrame.java
+++ src/br/joaovictor/swing/button/MyFrame.java
@@ -1,2 +1,34 @@
-package br.joaovictor.swing.button;public class MyFrame {
+package br.joaovictor.swing.button;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame implements ActionListener {
+    JButton botao;
+
+    public MyFrame() {
+        botao = new JButton();
+        botao.setBounds(200, 100, 100, 50);
+        botao.addActionListener(this);
+        botao.setText("Botão");
+        botao.setFocusable(false);//Para tirar uma borda que aparece circulando o texto do botão
+        //botao.setEnabled(false);//Ira deixar o botão desabilidado
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(500, 500);
+        this.setVisible(true);
+        this.add(botao);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==botao) {
+            System.out.println("poo");
+            botao.setEnabled(false);//Ira deixar o botão desabilidado
+        }
+
+    }
+
 }
